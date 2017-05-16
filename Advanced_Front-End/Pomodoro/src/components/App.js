793b90e5424111dc375cf9d77404d224041344b3
gitpash <arklogin@gmail.com>
2017-05-17 00:05:24 +0300

    add intervalId

--- Advanced_Front-End/Pomodoro/src/components/App.js
+++ Advanced_Front-End/Pomodoro/src/components/App.js
@@ -4,28 +4,42 @@ import PropTypes from "prop-types";
 class App extends React.Component {
   constructor() {
     super();
-    
+
     this.state = {
       displayCount: 25,
-      isRunning: "active"
+      isRunning: "active",
+      intervalId: ''
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleActive = this.handleActive.bind(this);
   }
   handleClick() {
     const { displayCount, isRunning } = this.state;
-    displayCount > 0
-      ? this.setState({
-          displayCount: displayCount - 1
-        })
-      : this.setState({ displayCount: 0 });
-      setInterval(this.handleClick, 1000)
+    
+       displayCount > 0
+          ? this.setState({
+              displayCount: displayCount - 1
+            })
+          : this.setState({ displayCount: 0 });
   }
-  
+
+  // handleActive() {
+  //   this.state.isRunning === "active"
+  //     ? this.setState({ isRunning: "inActive" })
+  //     : this.setState({ isRunning: "active" });
+  // }
+
   handleActive() {
-    this.state.isRunning === "active"
-      ? this.setState({ isRunning: "inActive" })
-      : this.setState({ isRunning: "active" });
+    const { isRunning } = this.state
+    if(isRunning === 'active') {
+      
+      let intervalId = setInterval(this.handleClick, 1000)
+      this.setState({isRunning: 'inActive', intervalId: intervalId})
+    }
+    else {
+      this.setState({isRunning: 'active'})
+      clearInterval(this.state.intervalId)
+    }
   }
   render() {
     const { displayCount, isRunning } = this.state;
@@ -33,11 +47,7 @@ class App extends React.Component {
       <div className="app-container">
         <div className="box-timer">
           <div
-            onClick={
-              isRunning === "active"
-                ? () => this.handleClick()
-                : () => this.handleActive()
-            }
+            onClick={() => this.handleActive()}
             className="circle"
           >
             <p>{displayCount}</p>
@@ -49,3 +59,10 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+  //  onClick={() => {
+  //             isRunning === "active"
+  //               ? setInterval(() => this.handleClick(), 1000)
+  //               : this.handleActive();
+  //           }}
